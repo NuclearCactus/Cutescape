b37caeba0c3dde12d7ab5e2dd8d237b5d27f2ac5
Soham Sen <soham2014sen@gmail.com>
2025-11-16 10:35:05 +0200

    Initial music working, main menu needs music, trigger needs to work

---
+++ Assets/Scripts/TriggerCheck.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine;
+
+public class TriggerCheck : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+            Debug.Log("Player triggered");
+    }
+}
